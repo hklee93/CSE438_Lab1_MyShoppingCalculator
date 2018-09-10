756bfe2421c417f8f528e7fbe5cab5fd901457ee
Hakkyung <hakkyung@wustl.edu>
2018-09-10 18:35:05 -0500

    Initial Commit

---
+++ Lab1-ShoppingCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab1-ShoppingCalculator
+//
+//  Created by Hakkyung on 2018. 9. 10..
+//  Copyright © 2018년 Hakkyung Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
